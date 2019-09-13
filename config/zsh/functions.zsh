20d76129cbd944c5976b4c47f06a806ea456a6f2
Harsha Ivaturi <isriharsha@atlassian.com>
2019-09-13 11:12:37 +1000

    Lazy load nvm

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -8,30 +8,3 @@ _has() {
   which $1>/dev/null 2>&1
 }
 
-# https://joshtronic.com/2018/10/05/speed-up-your-shell-prompt-when-usuing-nvm/
-# function nvm {
-#   if [ -z ${NVM_DIR+x} ]; then
-#     export NVM_DIR="$HOME/.nvm"
-#     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-#     nvm "$@"
-#   fi
-# }
-
-# Used to autoload nvm use. See https://github.com/nvm-sh/nvm#zsh
-load-nvmrc() {
-  local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
-
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$node_version" ]; then
-      nvm use
-    fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
-}
--- zshrc
+++ zshrc
@@ -29,11 +29,17 @@ if _has fasd; then
   eval "$(fasd --init auto)"
 fi
 
-export NVM_DIR="$HOME/.nvm"
-# This adds up to zsh load time. Find alternative
-source ~/.nvm/nvm.sh
 
-# Autoload nvm use
-autoload -U add-zsh-hook
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
+# Lazy load nvm. https://www.reddit.com/r/node/comments/4tg5jg/lazy_load_nvm_for_faster_shell_start/d5ib9fs
+declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+NODE_GLOBALS+=("node")
+NODE_GLOBALS+=("nvm")
+
+load_nvm () {
+  export NVM_DIR=~/.nvm
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+}
+
+for cmd in "${NODE_GLOBALS[@]}"; do
+  eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
+done
