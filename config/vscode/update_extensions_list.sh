f20ab4adf0ea5b54c15dbbd347b828f7a1a5c882
Harsha Ivaturi <harsha.ivaturi@gmail.com>
2023-11-20 11:00:07 +0530

    Install date time plugin for VS code

--- config/vscode/update_extensions_list.sh
+++ config/vscode/update_extensions_list.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Updating VS Code extensions"
-code --list-extensions > config/vscode/plugins.txt
+code --list-extensions > config/vscode/plugins.txt 
+sort config/vscode/plugins.txt
